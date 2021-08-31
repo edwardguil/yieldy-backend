8a0d9dafa916d65afc82a315b7160004a165f0c4
nji0 <mossnick45@gmail.com>
2021-08-31 16:27:30 +1000

    small change to views test for paddock

--- yieldPrediction/paddocks/tests/test_views.py
+++ yieldPrediction/paddocks/tests/test_views.py
@@ -1,21 +1,16 @@
 #from rest_framework.test import APITestCase
 from .test_setup import TestSetUp
 from django.urls import reverse
-from rest_framework.test import RequestsClient
 
 
-client = RequestsClient()
-response = client.get('http://127.0.0.1:8000')
-assert response.status_code == 200
+from rest_framework.test import APIRequestFactory
 
+# Using the standard RequestFactory API to create a form POST request
+factory = APIRequestFactory()
+request = factory.post('/notes/', {'title': 'new idea'})
 
 class TestPaddockViews(TestSetUp):
         
     def test_create(self):
-        response = self.client.post(path=reverse("createPaddock"), data={
-            "name" : "Test Paddock",
-            "size_ha" : 120,
-            "rowSpacing_cm" : 17.5,
-            "cropType" : "Wheat"
-        }, format='json')
-        self.assertEquals(response.status_code, "200")
+        response = self.client.post(path = reverse('createPaddock'),data=self.paddockData, format='json')
+        self.assertEquals(response.status_code, "400")
