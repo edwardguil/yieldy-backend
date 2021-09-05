751c24565a8b32cbe145ac0b86ca96f8cc345a45
Edward_Guilfoyle <edwardguil96@outlook.com>
2021-09-05 16:22:38 +1000

    Added new middleware for CORS

--- yieldPrediction/yieldPrediction/settings.py
+++ yieldPrediction/yieldPrediction/settings.py
@@ -44,14 +44,17 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'yieldPrediction.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'yieldPrediction.urls'
