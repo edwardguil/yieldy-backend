645502598e8da72163c19af9d07da693a9f352c3
Edward_Guilfoyle <edwardguil96@outlook.com>
2021-09-09 16:05:01 +1000

    Added a new way to runserver. Migrates & syncs DB on startup

---
+++ yieldPrediction/runserver.py
@@ -0,0 +1,9 @@
+import os
+
+def main(port=8000):
+    os.system('python manage.py makemigrations')
+    os.system('python manage.py migrate --run-syncdb')
+    os.system(f'python manage.py runserver {port}')
+
+if __name__ == '__main__':
+    main()
