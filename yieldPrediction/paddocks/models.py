3ba0dfa69a893bb4ad6196cd328d7644232891f8
edwardguil <edwardguil96@outlook.com>
2021-09-23 05:34:13 +0000

    Update models.py

--- yieldPrediction/paddocks/models.py
+++ yieldPrediction/paddocks/models.py
@@ -6,7 +6,7 @@ class Paddock(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
     cropType = models.CharField(max_length=128) #models.ForeignKey(Crop, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=128)
-    size_ha = models.IntegerField()
+    size_ha = models.FloatField()
     rowSpacing_cm = models.FloatField()
     grainsPerHead = models.FloatField(default=0)
     headsPerM2 = models.FloatField(default=0)
