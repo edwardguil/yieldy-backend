ddbd40c6a9200bb264e5f0465f67db2cc7aa3ee9
Nicholas Irving <mossnick45@gmail.com>
2021-10-28 01:37:00 +0000

    Update AdvancedModel.py

--- yieldPrediction/predictions/predictionModels/AdvancedModel.py
+++ yieldPrediction/predictions/predictionModels/AdvancedModel.py
@@ -5,9 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-/*
+"""
 * Data read-in and intial processing
-*/
+"""
 dftrain = pd.read_csv('~\\Toowoomba - Barley.csv')
 dfeval = pd.read_csv('~\\Toowoomba - Barley.csv')
 y_train = dftrain.pop('Yield')
@@ -15,16 +15,16 @@ y_eval = dfeval.pop('Yield')
 dftrain.pop('DISCARD')
 dfeval.pop('DISCARD')
 
-/*
+"""
 * Categorising the columns of data
-*/
+"""
 CATEGORICAL_COLUMNS = ['Month', 'Postcode']
 NUMERIC_COLUMNS = ['SolarExposure', 'Rainfall', 'Year']
 
 
-/*
+"""
 * processing each column according to it's category
-*/
+"""
 feature_columns = []
 for feature_name in CATEGORICAL_COLUMNS:
     vocabulary = dftrain[feature_name].unique()
@@ -33,9 +33,9 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
     feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
 
-/*
+"""
 * Input function generation
-*/
+"""
 def make_input_fn(data_df, label_df, num_epochs=1, shuffle=False, batch_size=32):
   def input_function():
     ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
@@ -45,23 +45,23 @@ def make_input_fn(data_df, label_df, num_epochs=1, shuffle=False, batch_size=32)
     return ds
   return input_function
 
-/*
+"""
 * Model training function assingnation
-*/
+"""
 train_input_fn = make_input_fn(dftrain, y_train)
 eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 
-/*
+"""
 * Defining the regression model that the data will be formatted to
-*/
+"""
 linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
 
-/*
+"""
 * Training the actual model
-*/
+"""
 linear_est.train(train_input_fn)
 
-/*
+"""
 * Once the model is trained this result can be acessed to get the predidction
-*/
+"""
 result = linear_est.evaluate(eval_input_fn)
